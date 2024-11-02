0590a95609cc812d28fff514d20568c0d744c44f
Djoseluis <dalbanojose@gmail.com>
2024-11-01 20:22:57 -0400

    ajuste de imagen en pestaña del navegador

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,7 +6,6 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>Rifa Rub&Maik</title>
-
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
